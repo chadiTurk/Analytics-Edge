828af8a96e39a2fd2261b66373c4d38e25d3e83d
Chadi <chadi_turk@dlsu.edu.ph>
2021-09-25 10:16:10 +0800

    completed analytical detective

---
+++ Analytical Detective/scriptFile.R
@@ -0,0 +1,14 @@
+#Script file for Analytical Detective
+
+#Reading the csv file
+
+Mvt = read.csv("mvtWeek1.csv")
+
+#Checking numerical statistics
+
+summary(Mvt)
+
+#Checking summary of table
+
+str(Mvt)
+
